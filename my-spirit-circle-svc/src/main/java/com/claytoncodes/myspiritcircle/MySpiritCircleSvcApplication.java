acaec748307462282168db6a992fff475395441e
cldaly <clayton.daly91@gmail.com>
2020-09-10 14:46:32 -0400

    initial commit

---
+++ my-spirit-circle-svc/src/main/java/com/claytoncodes/myspiritcircle/MySpiritCircleSvcApplication.java
@@ -0,0 +1,13 @@
+package com.claytoncodes.myspiritcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpiritCircleSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpiritCircleSvcApplication.class, args);
+	}
+
+}
---
+++ my-spirit-circle-svc/src/test/java/com/claytoncodes/myspiritcircle/MySpiritCircleSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytoncodes.myspiritcircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpiritCircleSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
